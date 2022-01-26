ee546dca64afb810a21051d1d45b8734d2f86646
mbeltagy <mohammed.elbeltagy@gmail.com>
2022-01-26 22:21:36 +0200

    Fixing file loading

--- src/data_setup.jl
+++ src/data_setup.jl
@@ -1,3 +1,4 @@
 
-data = CSV.read("data/word_freq_probs.csv",DataFrame)
+data = CSV.read(abspath(joinpath(@__DIR__, "..", "data", "word_freq_probs.csv")),DataFrame)
 words, freqs, depth_score, safety_score  = data.word, data.frequency, data.depth_score, data.safety_score
+word_freq_dict = Dict(zip(words,freqs))
